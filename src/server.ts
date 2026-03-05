a830e1b2018ff39a5acbdac52a0f80f9342d739b
sabjoao <sabino.jp03@gmail.com>
2026-03-05 02:00:57 -0300

    Criando a documentação da api

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,19 @@ import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import { router } from './routes';
 import response = require("express");
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
+import path from "path";
 
 const app = express();
 const port = 3333;
 
 app.use(express.json());
-app.use(router);
+app.use(cors());
+app.use("/v1", router);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof Error) {
@@ -20,6 +27,12 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
         message: 'Internal server error.'
     })
 })
+
+app.get("/terms",(request: Request, response: Response) => {
+    return response.json({
+        message: "Service terms"
+    })
+});
 
 app.listen(port, () => {
     console.log("Servidor rodando na porta 3333 + projeto Controle de Estoque curso Nodejs")
